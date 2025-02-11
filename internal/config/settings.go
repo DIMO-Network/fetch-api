32ac1a2fda32c49fd57c6ab1d43d88150e70d63b
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2025-02-11 16:24:00 -0500

    add VC bucket

--- internal/config/settings.go
+++ internal/config/settings.go
@@ -17,6 +17,7 @@ type Settings struct {
 	ChainID                   string          `yaml:"CHAIN_ID"`
 	CloudEventBucket          string          `yaml:"CLOUDEVENT_BUCKET"`
 	EphemeralBucket           string          `yaml:"EPHEMERAL_BUCKET"`
+	VCBucket                  string          `yaml:"VC_BUCKET"`
 	S3AWSRegion               string          `yaml:"S3_AWS_REGION"`
 	S3AWSAccessKeyID          string          `yaml:"S3_AWS_ACCESS_KEY_ID"`
 	S3AWSSecretAccessKey      string          `yaml:"S3_AWS_SECRET_ACCESS_KEY"`
