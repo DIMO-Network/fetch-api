7871a1a85727e2294ecd1670fcec7ef8bf7fb6bc
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2025-09-04 11:41:26 -0400

    fix: update env parsing for CH

--- internal/config/settings.go
+++ internal/config/settings.go
@@ -8,19 +8,19 @@ import (
 
 // Settings contains the application config.
 type Settings struct {
-	Port                      int             `env:"PORT"`
-	MonPort                   int             `env:"MON_PORT"`
-	GRPCPort                  int             `env:"GRPC_PORT"`
-	EnablePprof               bool            `env:"ENABLE_PPROF"`
-	TokenExchangeJWTKeySetURL string          `env:"TOKEN_EXCHANGE_JWK_KEY_SET_URL"`
-	TokenExchangeIssuer       string          `env:"TOKEN_EXCHANGE_ISSUER_URL"`
-	VehicleNFTAddress         common.Address  `env:"VEHICLE_NFT_ADDRESS"`
-	ChainID                   string          `env:"CHAIN_ID"`
-	CloudEventBucket          string          `env:"CLOUDEVENT_BUCKET"`
-	EphemeralBucket           string          `env:"EPHEMERAL_BUCKET"`
-	VCBucket                  string          `env:"VC_BUCKET"`
-	S3AWSRegion               string          `env:"S3_AWS_REGION"`
-	S3AWSAccessKeyID          string          `env:"S3_AWS_ACCESS_KEY_ID"`
-	S3AWSSecretAccessKey      string          `env:"S3_AWS_SECRET_ACCESS_KEY"`
-	Clickhouse                config.Settings `env:",inline"`
+	Port                      int            `env:"PORT"`
+	MonPort                   int            `env:"MON_PORT"`
+	GRPCPort                  int            `env:"GRPC_PORT"`
+	EnablePprof               bool           `env:"ENABLE_PPROF"`
+	TokenExchangeJWTKeySetURL string         `env:"TOKEN_EXCHANGE_JWK_KEY_SET_URL"`
+	TokenExchangeIssuer       string         `env:"TOKEN_EXCHANGE_ISSUER_URL"`
+	VehicleNFTAddress         common.Address `env:"VEHICLE_NFT_ADDRESS"`
+	ChainID                   string         `env:"CHAIN_ID"`
+	CloudEventBucket          string         `env:"CLOUDEVENT_BUCKET"`
+	EphemeralBucket           string         `env:"EPHEMERAL_BUCKET"`
+	VCBucket                  string         `env:"VC_BUCKET"`
+	S3AWSRegion               string         `env:"S3_AWS_REGION"`
+	S3AWSAccessKeyID          string         `env:"S3_AWS_ACCESS_KEY_ID"`
+	S3AWSSecretAccessKey      string         `env:"S3_AWS_SECRET_ACCESS_KEY"`
+	Clickhouse                config.Settings
 }
